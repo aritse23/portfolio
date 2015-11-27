d05dbc28e190d088cf4cec134487c6281a497712
Ari Tse <aritse23@gmail.com>
2015-11-27 16:08:20 +1300

    started coding jquery

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	// Clicking on project image
+	$('.proj-img').click(function(event){
+		// stops default action when clicking link
+		event.preventDefault();
+		// if image you clicked has the class landing, then show earth hour modal
+		if ($(this).hasClass('landing')){
+			$('#earth_hour').show('slow');
+			// click exit button to close modal
+			$('.exit').click(function(event){
+				event.preventDefault();
+				$('#earth_hour').hide('slow');
+			});
+		} else if ($(this).hasClass('movie')){
+			$('#inception').show('slow');
+			$('.exit').click(function(event){
+				event.preventDefault();
+				$('#inception').hide('slow');
+			});
+		} else if ($(this).hasClass('festival')){
+			$('#coca-cola').show('slow');
+			$('.exit').click(function(event){
+				event.preventDefault();
+				$('#coca-cola').hide('slow');
+			});
+		} else if ($(this).hasClass('brand')){
+			$('#air_jordan').show('slow');
+			$('.exit').click(function(event){
+				event.preventDefault();
+				$('#air_jordan').hide('slow');
+			});
+		}
+
+	});
+	// When clicking on nav item
+	
+});
